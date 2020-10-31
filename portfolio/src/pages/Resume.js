57d71d3e9250041e113ebc761e7fd7c9a10e4f42
Jacob Ashworth <jeashwor@gmail.com>
2020-10-31 00:43:50 -0400

    fixed pdf rendering issue

--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-// import PDF from "./JacobAshworthResume.pdf";
 
 function Resume() {
     return (
         <div>
-            <Document file="/JacobAshworthResume.pdf">
-                <Page pageNumber={1} />
-            </Document>
+            <div className="text-center">
+                <a href="/JacobAshworthResume.pdf" download>Download Link</a>
+            </div>
+            <div className="d-flex justify-content-center">
+                <Document file="/JacobAshworthResume.pdf">
+                    <Page pageNumber={1} />
+                </Document>
+            </div>
         </div>
+
     )
 }
 
